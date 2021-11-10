002518244034f019dabc8b32a1d3c1f59f16e36b
Victor Smith <victorj500@gmail.com>
2021-11-09 18:31:56 -0600

    created parameter scripts for candidates

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-const express = require('express');
+const mysql = require("mysql2");
+const express = require("express");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,18 +8,66 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    // Your MySQL username,
+    user: "root",
+    // Your MySQL password
+    password: "Rootsintheground21!",
+    database: "election",
+  },
+  console.log("Connected to the election database.")
+);
+
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//   console.log(rows);
+// });
+
+// // GET a single candidate
+// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(row);
+// });
+
+// // Delete a candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result);
+// });
+
+// Create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+              VALUES (?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+
+db.query(sql, params, (err, result) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(result);
+});
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
+
+
+
+
 // TEST route
 // app.get('/', (req, res) => {
 //   res.json({
 //     message: 'Hello World'
 //   });
 // });
-
-// Default response for any other request (Not Found)
-app.use((req, res) => {
-  res.status(404).end();
-})
-
-app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`);
-});
