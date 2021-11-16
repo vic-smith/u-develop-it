94ed99e11f3a9a77f043e4a89efd27ea2db073d4
Victor Smith <victorj500@gmail.com>
2021-11-15 19:06:29 -0600

    added voter routes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -2,5 +2,6 @@ const express = require('express');
 const router = express.Router();
 router.use(require('./partyRoutes'));
 router.use(require('./candidateRoutes'));
+router.use(require('./voterRoutes'));
 
 module.exports = router;
