811773038a60f252c9c556204d736f22c60d290f
Victor Smith <victorj500@gmail.com>
2021-11-15 19:44:18 -0600

    added votes table

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -3,5 +3,6 @@ const router = express.Router();
 router.use(require('./partyRoutes'));
 router.use(require('./candidateRoutes'));
 router.use(require('./voterRoutes'));
+router.use(require('./voteRoutes'));
 
 module.exports = router;
